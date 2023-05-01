aa413c1233d36faff828ea000b797f9a8a343cb4
DanBd22 <dan.badarau04@gmail.com>
2023-05-01 17:05:59 +0300

    Initial Commit

---
+++ ShopPage/ShopPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopPageApp.swift
+//  ShopPage
+//
+//  Created by Badarau Dan on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
